1e394804f3d3d8ce8eeb62364dd790642ff311f9
bilalba <tringingly@gmail.com>
2016-05-17 09:21:42 +0500

    rotate image done

---
+++ 48-RotateImage/rotateimage.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> A(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                A[i][j] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = A[n-1-j][i];
+            }
+        }
+    }
+};
