065a34033742e439cbb9e1f03d76453c0dc949fb
bilalba <tringingly@gmail.com>
2016-06-01 01:17:32 +0500

    fixed the solution with manual carry addition

--- 67-AddBinary/addBinary.cpp
+++ 67-AddBinary/addBinary.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        long a1 = 0;
-        long b1 = 0;
-        for (int i = 0; i < a.length(); i++) {
-            a1 <<= 1;
-            if (a[i] == '1')
-                a1 |= 1;
+        int a_length = a.length();
+        int b_length = b.length();
+        int min;
+        int max;
+        string longer;
+        string shorter;
+        if (a_length > b_length) {
+            max = a_length; longer = a;
+            min= b_length; shorter = b;
+        } else {
+            min = a_length; shorter = a;
+            max = b_length; longer = b;
         }
-        for (int i = 0; i <b.length(); i++) {
-            b1 <<= 1;
-            if (b[i] == '1')
-                b1 |= 1;
-        }
-        a1 += b1;
+        bool carry = false;
         string c = "";
-        if (a1 == 0)
-            return "0";
-        for (int i = 0; a1 != 0; i++) {
-            if (a1%2)
-             c= "1" + c;
-             else
-             c = "0" + c;
-             a1 >>= 1;
+        for (int i = 0; i < min; i++) {
+            int min_i = min -1-i;
+            int max_i = max -1-i;
+            if ((shorter[min_i] == '1') && (longer[max_i] == '1')) {
+                if (carry)
+                    c = '1' + c;
+                else
+                 c = '0' + c;
+                 carry = true;
+            } else if((shorter[min_i] == '1') || (longer[max_i] == '1')) {
+                if (carry) {
+                    c = '0' + c;
+                    carry = true;
+                } else {
+                     c = '1' + c;
+                     carry = false;
+                }
+                    
+            } else {
+                if (carry)
+                    c = '1' + c;
+                else
+                 c = '0' + c;
+                carry = false;
+            }
         }
-        return c;
-    }
-};
+        
+        for (int i = 0; i < max -min; i++) {
+            int index = max-min -1 - i;
+            if (longer[index] == '1') {
+                if (carry) {
+                    c = '0' + c;
+                    // carry = true;
+                } else {
+                    c = '1' + c;
+                    carry = false;
+                }
+
