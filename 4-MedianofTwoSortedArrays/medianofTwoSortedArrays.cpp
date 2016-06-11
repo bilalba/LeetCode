7b281a5053c8f349b426cd122bc74b65bab9bda0
bilalba <tringingly@gmail.com>
2016-06-11 13:12:04 +0500

    completed problem

--- 4-MedianofTwoSortedArrays/medianofTwoSortedArrays.cpp
+++ 4-MedianofTwoSortedArrays/medianofTwoSortedArrays.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
 
+    // int findKth()
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int one = nums1.size();
         int two = nums2.size();
         if (one == 0 && two == 0)
-            // cout << "LOLL";
             return 0;
+            
+        if (one == 1 && two == 1)
+            return (nums1[0] + nums2[0])/2.0;
+            
         else if (one == 0 || two == 0) {
             if (one == 0) {
                 if (two %2 == 1)
@@ -21,9 +25,6 @@ public:
                     return (nums1[(one-1)/2] + nums1[(one)/2])/2.0;
             }
         }
-        // else.
-        
-    
         vector<int> *big;
         vector<int> *small;
         int large;
@@ -35,56 +36,49 @@ public:
             big= &nums2; large = two;
             small = &nums1; tiny = one;
         }
-        
-        // check if 
-        
-        
-        
         int total = one + two;
-        if ((total)%2 == 0) { // even number.
-            // find the (t-1)/2
-            
-            // find the (t)/2
-            
-            
-        }  // odd numb.
+          // odd numb.
             // find the (t)/2 index.
         bool turn = false; // element from large array.
         int ind = large/2;
-        int elem = *(big)[ind];
+        int elem = (*big)[ind];
+        int smallboy = 0;
         while (true) {
+         
             if (!turn) {
-                elem = *(big)[ind];
+                elem = (*big)[ind];
                 int other_ind = total/2 - ind;
-                if (other_ind != 0) // necessary to not make invalid comparison.
-                    if (*(small)[other_ind-1] > elem ) { // we have a wrong element. 
+                if (other_ind > 0) // necessary to not make invalid comparison.
+                    if ((*small)[other_ind-1] > elem ) { // we have a wrong element. 
                         turn = !turn;
                         ind = other_ind-1;
                         continue;
+                    } else {
+                        smallboy =(*small)[other_ind-1];
                     }
                         // flip side and choose this element.
-                if (other_ind != tiny) // necessary to not make invalid comparison.
-                    if (*(small)[other_ind] < elem ) {
+                if (other_ind < tiny) // necessary to not make invalid comparison.
+                    if ((*small)[other_ind] < elem ) {
                         turn = !turn;//flip side and choose this elemnt
                         ind = other_ind;
                         continue;
                     }
                 // we have found correct element and we r da winners. 
                 break;
-                    
-                        .
             } else {
-                elem = *(small)[ind];
+                elem = (*small)[ind];
                 int other_ind = total/2 - ind;
-                if (other_ind != 0) // necessary to not make invalid comparison.
-                    if (*(big)[other_ind-1] > elem ) { // we have a wrong element. 
+                if (other_ind > 0) // necessary to not make invalid comparison.
+                    if ((*big)[other_ind-1] > elem ) { // we have a wrong element. 
                         turn = !turn;
                         ind = other_ind-1;
                         continue;
+                    } else {
+                        smallboy = (*big)[other_ind-1];
                     }
                         // flip side and choose this element.
-                if (other_ind != tiny) // necessary to not make invalid comparison.
-                    if (*(big)[other_ind] < elem ) {
+                if (other_ind < large) // necessary to not make invalid comparison.
+                    if ((*big)[other_ind] < elem ) {
                         turn = !turn;//flip side and choose this elemnt
                         ind = other_ind;
                         continue;
@@ -94,7 +88,27 @@ public:
             }
             
         }
-        
-        return 0;
+        if ((total)%2 == 0) { // even number.
+              if (!turn) {
+                if (ind > 0) {
+                    if (smallboy > (*big)[ind-1]){
+                        return (smallboy + elem)/2.0;
+                     }else {
+                        return ((*big)[ind-1] + elem)/2.0;}
+                } else {
+                    return (smallboy + elem)/2.0;
+                }
+            } else {
+                if (ind > 0) {
+                    if (smallboy > (*small)[ind-1]){
+                        return (smallboy + elem)/2.0;
+                     }else {
+                        return ((*small)[ind-1] + elem)/2.0;}
+                } else {
+                    return (smallboy + elem)/2.0;
+                }
+            }
+        }
+        return elem;
     }
 };
