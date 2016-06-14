85aa933614c25af4e53e08c338decc635183156d
bilalba <tringingly@gmail.com>
2016-06-14 14:10:22 +0500

    Solved, using recursion, and count of open and closed brackets. binary tree

--- 22-GenerateParenthesis/generateParenthesis.cpp
+++ 22-GenerateParenthesis/generateParenthesis.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-         vector<string> a;
-        if (n >= 1)
-        a = {"()"};
-        int start = 0;
-        int length = 1;
-        if (n > 1) {
-            for (int i = 1; i < n; i++) {
-                int count = 0;
-                for (int k = 0; k < length; length--) {
-                    string abc ="("+ a[0] + ")";
-                    a.push_back(abc);
-                    count++;
-                    string one = "()" + a[0];
-                    string two = a[0] + "()";
-                    if (one == two) {
-                        a.push_back(one);
-                        count++;
-                    } else {
-                          a.push_back(one);
-                        a.push_back(two);
-                        count += 2;
-                    }
-                    a.erase(a.begin());
-                    
-                }
-                length = count;
-            }
+        return recursion(n,n,"");
+    }
+    
+    vector<string> recursion(int open, int closed,string str) {
+        vector<string> a, b;
+        if (closed == 0){
+            a.push_back(str);
+            return a;
         }
-        return a;
+        if (open == closed) {
+            str += "(";
+            open--;
+        }
+        
+        if (closed > open) {
+            if (open > 0)
+                b =recursion( open-1, closed, str+"(");
+            a = recursion(open, closed-1, str+")");
+            
+                
+        }
+        for (int i = 0; i < a.size(); i++) {
+            b.push_back(a[i]);
+        }
+        return b;
+        
+        
     }
 };
